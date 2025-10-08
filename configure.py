12774e8641fd496516741e66d7c5e052abc31322
Luke Street <luke@street.dev>
2025-10-07 20:01:10 -0600

    CI maybe works (#37)
    * Update to wibo 1.0.0-alpha.1 and rewrite transform_dep.py
    
    * Enable CI workflow & update compilers, project.py
    
    * Remove -v from ninja args

--- configure.py
+++ configure.py
@@ -172,11 +172,11 @@ if not config.non_matching:
 
 # Tool versions
 config.binutils_tag = "2.42-1"
-config.compilers_tag = "20250725"
+config.compilers_tag = "20250812"
 config.dtk_tag = "v0.1.1"
 config.objdiff_tag = "v3.2.1"
 config.sjiswrap_tag = "v1.2.1"
-config.wibo_tag = "0.6.16"
+config.wibo_tag = "1.0.0-alpha.1"
 
 # Project
 config_dir = Path("config") / config.version
