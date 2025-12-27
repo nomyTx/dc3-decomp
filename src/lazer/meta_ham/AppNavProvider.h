4212dc6b7aed3710040fdfb753e42577b8c345a0
Jacob Senior <32003487+jsenior10@users.noreply.github.com>
2025-12-27 17:08:48 +0000

    some cheatsmanager, weightinput, hamscreen, appnavprovider (#110)

---
+++ src/lazer/meta_ham/AppNavProvider.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "hamobj/HamNavProvider.h"
+#include "ui/UIListLabel.h"
+#include "obj/Object.h"
+
+class AppNavProvider : public HamNavProvider {
+    public:
+    AppNavProvider() {};
+    ~AppNavProvider();
+    OBJ_CLASSNAME(AppNavProvider);
+    OBJ_SET_TYPE(AppNavProvider);
+    virtual void Text(int, int, UIListLabel *, UILabel *) const;
+    virtual void Custom(int, int, UIListCustom *, Hmx::Object *) const;
+    virtual DataNode Handle(DataArray *, bool);
+
+};
