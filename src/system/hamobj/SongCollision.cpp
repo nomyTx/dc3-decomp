d5b59b0cfeca02dddaaa0998f88e02840c761dbc
rjkiv <76180273+rjkiv@users.noreply.github.com>
2026-02-01 13:51:55 -0700

    songcollision

--- src/system/hamobj/SongCollision.cpp
+++ src/system/hamobj/SongCollision.cpp
@@ -1,12 +1,49 @@
 #include "hamobj/SongCollision.h"
 #include "SongCollision.h"
 #include "hamobj/Difficulty.h"
+#include "math/Vec.h"
 #include "obj/Data.h"
 #include "obj/DataUtl.h"
 #include "obj/Object.h"
+#include "utl/BinStream.h"
 #include "utl/Std.h"
 
 std::vector<const char *> sCollisionUsefulBoneNames;
+
+#pragma region BeatCollisionData
+
+void BeatCollisionData::Set(
+    float minX, float minY, const Transform &start_xfm, const Transform &end_xfm
+) {
+    using namespace Hmx;
+    MILO_ASSERT(start_xfm.m == Matrix3::GetIdentity(), 0x62);
+    MILO_ASSERT(end_xfm.m == Matrix3::GetIdentity(), 0x63);
+    mMinX = minX;
+    mMaxX = minY;
+    Subtract(start_xfm.v, end_xfm.v, mOffset);
+}
+
+BinStream &operator<<(BinStream &bs, const BeatCollisionData &bcd) {
+    bs << bcd.mMinX << bcd.mMaxX;
+    bs << bcd.mOffset;
+    return bs;
+}
+
+BinStreamRev &operator>>(BinStreamRev &d, BeatCollisionData &bcd) {
+    d >> bcd.mMinX;
+    d >> bcd.mMaxX;
+    if (d.rev > 1) {
+        d >> bcd.mOffset;
+    } else if (d.rev > 0) {
+        Transform xfm;
+        d >> xfm;
+        bcd.mOffset = xfm.v;
+    }
+    return d;
+}
+
+#pragma endregion
+#pragma region SongCollision
 
 SongCollision::SongCollision() {}
 
@@ -21,18 +58,39 @@ BEGIN_PROPSYNCS(SongCollision)
     SYNC_SUPERCLASS(Hmx::Object)
 END_PROPSYNCS
 
+BEGIN_SAVES(SongCollision)
+    SAVE_REVS(2, 1)
+    SAVE_SUPERCLASS(Hmx::Object)
+    for (int i = 0; i < kNumDifficulties; i++) {
+        bs << mData[i];
+    }
+END_SAVES
+
 BEGIN_COPYS(SongCollision)
-COPY_SUPERCLASS(Hmx::Object)
-CREATE_COPY(SongCollision)
-BEGIN_COPYING_MEMBERS
-//COPY_MEMBER_FROM(c, mData)
-END_COPYING_MEMBERS
+    COPY_SUPERCLASS(Hmx::Object)
+    CREATE_COPY(SongCollision)
+    BEGIN_COPYING_MEMBERS
+        for (int i = 0; i < kNumDifficulties; i++) {
+            COPY_MEMBER(mData[i])
+        }
+    END_COPYING_MEMBERS
 END_COPYS
 
-BEGIN_SAVES(SongCollision)
-SAVE_REVS(2, 1)
-SAVE_SUPERCLASS(Hmx::Object)
-END_SAVES
+BEGIN_LOADS(SongCollision)
+    LOAD_REVS(bs)
+    ASSERT_REVS(2, 1)
+    LOAD_SUPERCLASS(Hmx::Object)
+    if (d.rev < 1) {
+        for (int i = 0; i < kNumDifficultiesDC2; i++) {
+            d >> mData[i];
+        }
+        mData[kDifficultyBeginner] = mData[kDifficultyEasy];
+    } else {
+        for (int i = 0; i < kNumDifficulties; i++) {
+            d >> mData[i];
+        }
+    }
+END_LOADS
 
 void SongCollision::Print() {
     int maxDatas = 0;
@@ -46,7 +104,47 @@ void SongCollision::Print() {
         str += MakeString("\t%s", DifficultyToSym(d).Str());
     }
     MILO_LOG("%s\n", str.c_str());
-    // more...
+    for (int i = 0; i < maxDatas; i++) {
+        BeatCollisionData allDatas[kNumDifficulties];
+        BeatCollisionData *dataIt = allDatas;
+        for (int j = 0; j < kNumDifficulties; j++, dataIt++) {
+            if (i < mData[j].size()) {
+                *dataIt = mData[j][i];
+            } else {
+                memset(dataIt, 0, sizeof(BeatCollisionData));
+            }
+        }
+        str = MakeString("%d\tMin X", i);
+        for (Difficulty d = EasiestDifficulty(); d < kNumDifficulties;
+             d = DifficultyOneHarder(d)) {
+            str += MakeString("\t%f", allDatas[d].mMinX);
+        }
+        MILO_LOG("%s\n", str.c_str());
+        str = "\tMax X";
+        for (Difficulty d = EasiestDifficulty(); d < kNumDifficulties;
+             d = DifficultyOneHarder(d)) {
+            str += MakeString("\t%f", allDatas[d].mMaxX);
+        }
+        MILO_LOG("%s\n", str.c_str());
+        str = "\tOffset X";
+        for (Difficulty d = EasiestDifficulty(); d < kNumDifficulties;
+             d = DifficultyOneHarder(d)) {
+            str += MakeString("\t%f", allDatas[d].mOffset.x);
+        }
+        MILO_LOG("%s\n", str.c_str());
+        str = "\tOffset Y";
+        for (Difficulty d = EasiestDifficulty(); d < kNumDifficulties;
+             d = DifficultyOneHarder(d)) {
+            str += MakeString("\t%f", allDatas[d].mOffset.y);
+        }
+        MILO_LOG("%s\n", str.c_str());
+        str = "\tOffset Z";
+        for (Difficulty d = EasiestDifficulty(); d < kNumDifficulties;
+             d = DifficultyOneHarder(d)) {
+            str += MakeString("\t%f", allDatas[d].mOffset.z);
+        }
+        MILO_LOG("%s\n", str.c_str());
+    }
 }
 
 void SongCollision::Init() {
@@ -66,26 +164,11 @@ void SongCollision::Init() {
 
 const BeatCollisionData *SongCollision::BeatData(int beat, Difficulty diff) const {
     MILO_ASSERT_RANGE(diff, 0, kNumDifficulties, 0xe8);
+    const std::vector<BeatCollisionData> &diffData = mData[diff];
     MILO_ASSERT(beat >= 0, 0xeb);
-    if (beat < mData[diff].size()) {
-        return &mData[diff][beat];
+    if (beat < diffData.size()) {
+        return &diffData[beat];
+    } else {
+        return nullptr;
     }
-    return nullptr;
-}
-
-void BeatCollisionData::Set(float minX, float minY, const Transform &start_xfm, const Transform &end_xfm) {
-    using namespace Hmx;
-    MILO_ASSERT(start_xfm.m == Matrix3::GetIdentity(), 0x62);
-    MILO_ASSERT(end_xfm.m == Matrix3::GetIdentity(), 0x63);
-    mMinX = minX;
-    mMaxX = minY;
-    float startXX = start_xfm.m.x.x;
-    float endXX = end_xfm.m.x.x;
-    float startXY = start_xfm.m.x.y;
-    float endXY = end_xfm.m.x.y;
-    float startXZ = start_xfm.m.x.z;
-    float endXZ = end_xfm.m.x.z;
-    mOffset.x = startXX - endXX;
-    mOffset.y = startXY - endXY;
-    mOffset.z = startXZ - endXZ;
 }
--- src/system/hamobj/SongCollision.h
+++ src/system/hamobj/SongCollision.h
@@ -1,17 +1,22 @@
 #pragma once
 #include "MoveDir.h"
 #include "hamobj/Difficulty.h"
+#include "hamobj/HamCharacter.h"
+#include "math/Mtx.h"
 #include "math/Vec.h"
 #include "obj/Object.h"
+#include "rndobj/Trans.h"
 #include "utl/MemMgr.h"
 
 struct BeatCollisionData {
     void Set(float, float, const Transform &, const Transform &);
 
-    float mMinX;
-    float mMaxX;
-    Vector3 mOffset;
+    float mMinX; // 0x0
+    float mMaxX; // 0x4
+    Vector3 mOffset; // 0x8
 };
+
+struct SongCollisionOutput {};
 
 class SongCollision : public Hmx::Object {
 public:
@@ -28,16 +33,32 @@ public:
     OBJ_MEM_OVERLOAD(0x2D)
     NEW_OBJ(SongCollision)
 
-    static void Init();
     void Update(MoveDir *);
     bool Equals(SongCollision *);
+    bool IsCollision(
+        int,
+        int,
+        const Difficulty *const,
+        const Transform *const,
+        std::vector<SongCollisionOutput> *
+    ) const;
+
+    static void Init();
+    static void GatherUsefulBones(std::vector<RndTransformable *> &, HamCharacter *);
 
 private:
     static float sCollisionTolerance;
+
     const BeatCollisionData *BeatData(int, Difficulty) const;
+    void CheckCollision(
+        int, const Difficulty *const, const Transform *const, SongCollisionOutput &
+    ) const;
 
 protected:
     SongCollision();
 
+    // indexed by difficulty, then beat
+    // so mData[kDifficultyExpert][4] is
+    // the BeatCollisionData for expert difficulty at beat 4
     std::vector<BeatCollisionData> mData[kNumDifficulties]; // 0x2c
 };
